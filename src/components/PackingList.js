dfa9ce55911a140a32392bfadefc0c6c40b067b5
ManojMaheshPatil <patil.mahesh.manoj@gmail.com>
2023-08-29 11:59:16 +0530

    First commit

---
+++ src/components/PackingList.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import Item from "./Item";
+
+export default function PackingList({
+  items,
+  onDeleteItem,
+  onToggleItem,
+  onClearList,
+}) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") {
+    sortedItems = items;
+  }
+
+  if (sortBy === "description") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+    // The reason why we use slice() is because , the sort() function will sort the input array , which we should not do. We should never mutate the input array. Hence we make a copy of it using slice() first and then use sort on it.
+  }
+
+  if (sortBy === "packed") {
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
+  }
+  return (
+    <div className="list">
+      <ul>
+        {sortedItems.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order </option>
+          <option value="description">Sort by description </option>
+          <option value="packed">Sort by status </option>
+        </select>
+        <button onClick={onClearList}>Clear List</button>
+      </div>
+    </div>
+  );
+}
